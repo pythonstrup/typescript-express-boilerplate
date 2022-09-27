80607305a91d13962dfc9aa2b9daa9090045da66
Gakko <pythonstrup@gmail.com>
2022-09-27 14:07:05 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log('my Server!!');
+});
